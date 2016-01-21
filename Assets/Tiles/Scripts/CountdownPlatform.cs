b07295bee5ca5affc026eacaffcca8aaf1b65430
stshing7609 <stshing7609@gmail.com>
2016-01-21 16:08:58 -0500

    Platforms 1
    Moved player control and movement and moving platforms to Update rather
    than FixedUpdate as we're not using Rigidbodies and the physics engine.
    Added phase-able platforms and a countdown platform (destroys itself a
    set time after the player steps on it).
    Added the RayLayers static class to hold info on LayerMasks the player
    will interact with.
    Made the skin variable in PlayerPhysics static to allow other classes to
    access it without created a reference of the object.

---
+++ Assets/Tiles/Scripts/CountdownPlatform.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountdownPlatform : MonoBehaviour {
+
+    private const int k_collisionsDivisionX = 10;
+    private BoxCollider2D collider;
+    private Vector2 p;              // our current position
+    private Vector2 s;              // the size of our collider
+    private Vector2 o;              // the center of our collider
+    private const float k_distCheck = .2f;
+
+    // used if we have scaled the collider
+    private Vector2 originalSize;   // holds the original size of the collider
+    private Vector2 originalOffset; // holds the original offset of the collider
+    private Vector2 colliderScale;    // how much have we scaled by
+
+    // Use this for initialization
+    void Start () {
+        collider = GetComponent<BoxCollider2D>();
+        p = transform.position;
+        colliderScale = transform.localScale;
+        originalSize = collider.size;
+        originalOffset = collider.offset;
+        SetCollider(originalSize, originalOffset);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < k_collisionsDivisionX; i++)
+        {
+            // split apart the rays staring from the left of the box to the right of the box
+            float x = (p.x + o.x - s.x / 2) + s.x / (k_collisionsDivisionX - 1) * i;
+            float y = p.y + o.y + s.y / 2; // top edge of collider
+
+            Vector2 rayOrigin = new Vector2(x, y);  // set the ray's origin point
+
+            // THE FOLLOWING IS USED FOR DEBUGGING
+            Ray2D ray = new Ray2D(rayOrigin, new Vector2(0, 1));
+            Debug.DrawRay(ray.origin, ray.direction, Color.cyan);
+
+            // set up our RaycastHit2d
+            //@params: the origin we set up earlier
+            //         the direction determined by our calculation earlier
+            //         only check as far as how far the player would move
+            //         the collisionLayerMask
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, new Vector2(0, 1), k_distCheck, LayerMask.NameToLayer("Player"));
+            //did we generate a hit
+            if (hit.fraction > 0)
+            {
+                Debug.Log("Destroy Running");
+                Destroy(gameObject, 3);
+            }
+        }
+    }
+
+    // sets up our collider
+    public void SetCollider(Vector2 size, Vector2 offset)
+    {
+        collider.size = size;
+        collider.offset = offset;
+
+        // scale the values used to set up raycasting
+        s = Vector2.Scale(size, colliderScale) + new Vector2(PlayerPhysics.skin, PlayerPhysics.skin);
+        o = Vector2.Scale(offset, colliderScale);
+    }
+}
--- Assets/Tiles/Scripts/MovingPlatform.cs
+++ Assets/Tiles/Scripts/MovingPlatform.cs
@@ -1,35 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovingPlatform : MonoBehaviour {
+public class MovingPlatform : MonoBehaviour
+{
 
     public GameObject platform;         // The platform in the game world that will be moving
     public float moveSpeed;             // The speed of the platform's movement
     private Transform targetPoint;      // The point that the platform is currently moving to
     public Transform[] points;          // All the points the platform will move through
-    public int pointSelection;          // What point in the array are we looking at
+    public int currPoint;          // What point in the array are we looking at
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         // Set the first point that we'll be moving towards
-        targetPoint = points[pointSelection];
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+        targetPoint = points[currPoint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         // Moving the platform from its position to the targetPoint at moveSpeed.
         platform.transform.position = Vector3.MoveTowards(platform.transform.position, targetPoint.position, Time.deltaTime * moveSpeed);
 
         // Switch to the next targetPoint if we've reached one
-        if(platform.transform.position == targetPoint.position)
+        if (platform.transform.position == targetPoint.position)
         {
-            pointSelection++;
+            currPoint++;
 
             // If we've reached the endPoint, continue forward to the first point.
-            if (pointSelection == points.Length)
-                pointSelection = 0;
+            if (currPoint == points.Length)
+                currPoint = 0;
 
-            targetPoint = points[pointSelection];
+            targetPoint = points[currPoint];
         }
     }
 }
+
