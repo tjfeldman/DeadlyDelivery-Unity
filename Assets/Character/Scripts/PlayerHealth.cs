c382ca3d97227eb346603c552f72a1a3a23f13c7
Tyler Feldman <tmaster148@gmail.com>
2016-02-19 02:27:22 -0500

    Healthbar and PlayerHealth controller
    Playerhealth controller classed created. A HUD element added with a
    healthbar. The healthbar will change when the player is damaged.

---
+++ Assets/Character/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider healthbar;
+    private float currentHealth, maxHealth;
+
+    // Use this for initialization
+    void Awake()
+    {
+        currentHealth = maxHealth = 100;//starting health is 100
+        takeDamage(15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Reduces health based on damage taken
+    //@param damage - damage taken
+    public void takeDamage(float damage)
+    {
+        currentHealth -= damage;
+        //health can't be a negative number
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        healthbar.value = getHealthRemaining();//adjust the healthbar display now that health has been changed
+    }
+
+    //Returns the percentage of health remaining
+    private float getHealthRemaining()
+    {
+        return ((currentHealth / maxHealth) * 100);
+    }
+}
--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
     public GameObject player;
+    public Slider healthbar;
     public Vector3 startSpawn;
     private GameCamera cam;
 
@@ -13,10 +15,11 @@ public class GameManager : MonoBehaviour {
         startSpawn = new Vector3(0, 0, 0);
         SpawnPlayer();
 	}
-	
+
 	private void SpawnPlayer()
     {
         // set our camera's target to the player as well as spawn the player
         cam.SetTarget((Instantiate(player, startSpawn, Quaternion.identity) as GameObject).transform);
+        player.GetComponent<PlayerHealth>().healthbar = healthbar;
     }
 }
